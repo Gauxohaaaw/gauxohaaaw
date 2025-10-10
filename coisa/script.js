93758af897db4c4e1c2330713fce5ca6e4ff1fc2
Gauxohaaaw <luis.vesolovski.ribeiro@escola.pr.gov.br>
2025-10-10 13:29:59 -0300

    .

--- coisa/script.js
+++ coisa/script.js
@@ -1,11 +1,12 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-       button.addEventListener('click', function(){
+     button.addEventListener('click', function(){
      const atual = Document.querySelector('.ativo')
 
 
-
+     atual.classlist.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
 
 
 
